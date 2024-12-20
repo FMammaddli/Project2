435073f24bf7166fc9666c3ef0451bd571b721bf
FMammaddli <fmammadli13936@ada.edu.az>
2024-12-20 19:43:48 +0400

    Commited css

---
+++ src/pages/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from "./HomePage";
+import RecipePage from "./RecipePage";
+import ContactPage from "./ContactPage";
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe" element={<RecipePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </Router>
+  );  
+}
+
+export default App;
