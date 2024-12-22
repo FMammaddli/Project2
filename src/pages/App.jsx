78fc3eed5b06226df1d7b741d27ae612f76d6a68
Ibrahim2307 <aibrahim.0505@gmail.com>
2024-12-22 23:59:03 +0400

    Tailwind styling added

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,19 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import HomePage from "./HomePage";
-import RecipePage from "./RecipePage";
-import ContactPage from "./ContactPage";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './HomePage';
+import RecipePage from './RecipePage';
+import ContactPage from './ContactPage';
 
-const App = () => {
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/recipes" element={<RecipePage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
+      <div className="min-h-screen bg-zinc-950">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recipe" element={<RecipePage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
     </Router>
   );
-};
+}
+
 
 export default App;
